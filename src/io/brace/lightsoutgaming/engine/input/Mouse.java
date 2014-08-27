c233b5259328e793d90a565c40861f0b52942d41
taz40 <ldspcjunkie@gmail.com>
2014-08-26 18:25:29 -0600

    8/26/14 6:23 pm. mouse movement addition.

--- src/io/brace/lightsoutgaming/engine/input/Mouse.java
+++ src/io/brace/lightsoutgaming/engine/input/Mouse.java
@@ -1,6 +1,7 @@
 package io.brace.lightsoutgaming.engine.input;
 
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 /**
  * used to track mouse pos, and button state.
@@ -9,7 +10,7 @@ import java.awt.event.MouseEvent;
  */
 import java.awt.event.MouseListener;
 
-public class Mouse implements MouseListener{
+public class Mouse implements MouseListener, MouseMotionListener{
 	/**
 	 * the X position of the mouse.
 	 */
@@ -18,6 +19,14 @@ public class Mouse implements MouseListener{
 	 * the Y position of the mouse.
 	 */
 	public static int mouseY;
+	/**
+	 * the X position of the mouse last click.
+	 */
+	public static int clickX;
+	/**
+	 * the Y position of the mouse last click.
+	 */
+	public static int clickY;
 	/**
 	 * the currently clicked mouse button.
 	 */
@@ -40,13 +49,22 @@ public class Mouse implements MouseListener{
 	public void mousePressed(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		button = arg0.getButton();
-		mouseX = arg0.getX();
-		mouseY = arg0.getY();
+		clickX = arg0.getX();
+		clickY = arg0.getY();
 	}
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
 		button = 0;
+	}
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
 		mouseX = arg0.getX();
 		mouseY = arg0.getY();
 	}
